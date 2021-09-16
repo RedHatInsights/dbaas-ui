eb8b80ea6c3f7b3d6e9f79700ae201eb38505d6e
Martin Maroši <marvusm.mmi@gmail.com>
2021-09-16 09:34:30 +0200

    Update app name.

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -8,7 +8,7 @@ const { config: webpackConfig, plugins } = config({
   deployment: process.env.BETA ? 'beta/apps' : 'apps',
   useProxy: true,
   useCloud: true, // Until console.redhat.com is working
-  appUrl: process.env.BETA ? '/beta/staging/starter' : '/staging/starter',
+  appUrl: process.env.BETA ? '/beta/application-services/dbaas' : '/application-services/dbaas',
   env: process.env.BETA ? 'ci-beta' : 'ci-stable',
   standalone: Boolean(process.env.STANDALONE),
 });
