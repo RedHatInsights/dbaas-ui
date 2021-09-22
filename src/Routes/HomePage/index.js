d12515edd508be467d8708a688d32cf8a3684b37
Martin Maroši <marvusm.mmi@gmail.com>
2021-09-22 09:26:29 +0200

    Add videos to homepage.

--- src/Routes/HomePage/index.js
+++ src/Routes/HomePage/index.js
@@ -7,9 +7,8 @@ import {
   TextContent,
   Grid,
   GridItem,
-  CardBody,
-  Card,
-  Bullseye,
+  Stack,
+  StackItem,
 } from '@patternfly/react-core';
 import { Main } from '@redhat-cloud-services/frontend-components/Main';
 import { PageHeader } from '@redhat-cloud-services/frontend-components/PageHeader';
@@ -45,6 +44,7 @@ const HomePage = () => {
                 component="a"
                 target="_blank"
                 variant="secondary"
+                href="https://access.redhat.com/documentation/en-us/red_hat_managed_data_services/1/html-single/red_hat_openshift_database_access_quick_start_guide/index"
               >
                 Get Started with accessing databases from OpenShift
               </Button>
@@ -56,46 +56,82 @@ const HomePage = () => {
         <Title className="pf-u-mb-xl" headingLevel="h2" size="xl">
           Demo of OpenShift Database Access
         </Title>
-        <Grid hasGutter>
-          <GridItem md={6} sm={12}>
-            <TextContent>
-              <Text>
-                In this video you’ll learn how as a database admin you can
-                assign your purchased MongoDB Atlas and/or Crunchy Bridge
-                managed database service to your managed OpenShift Cluster.
-              </Text>
-            </TextContent>
-            <Button
-              iconPosition="right"
-              icon={<ExternalLinkAltIcon />}
-              target="_blank"
-              className="pf-u-mt-xl"
-              component="a"
-              variant="secondary"
-            >
-              View the demo
-            </Button>
-          </GridItem>
-          <GridItem md={6} sm={12}>
-            <Card>
-              <CardBody>
-                <Bullseye className="dbaas-home-page__video-card">
-                  <div>
-                    <Title headingLevel="h3" size="lg">
-                      Video is comming soon
-                    </Title>
-                    <Bullseye>
-                      <VideoIcon
-                        size="xl"
-                        color="var(--pf-global--danger-color--100)"
-                      />
-                    </Bullseye>
-                  </div>
-                </Bullseye>
-              </CardBody>
-            </Card>
-          </GridItem>
-        </Grid>
+        <Stack hasGutter>
+          <StackItem>
+            <Grid hasGutter>
+              <GridItem md={6} sm={12}>
+                <TextContent>
+                  <Text>
+                    <b>For database admins.</b> In this video you’ll learn how
+                    as a database admin you can access your purchased MongoDB
+                    Atlas and/or Crunchy Bridge managed database service to your
+                    managed OpenShift Cluster.
+                  </Text>
+                </TextContent>
+                <Button
+                  iconPosition="right"
+                  icon={<ExternalLinkAltIcon />}
+                  target="_blank"
+                  className="pf-u-mt-xl"
+                  component="a"
+                  variant="secondary"
+                  href="https://youtu.be/QmF5da2LvnU "
+                >
+                  View the demo
+                </Button>
+              </GridItem>
+              <GridItem md={6} sm={12}>
+                <iframe
+                  style={{ width: '100%', height: '315px' }}
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/QmF5da2LvnU"
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </GridItem>
+            </Grid>
+          </StackItem>
+          <StackItem>
+            <Grid hasGutter>
+              <GridItem md={6} sm={12}>
+                <TextContent>
+                  <Text>
+                    <b>For database developers.</b> In this video you’ll learn
+                    how as a developer you can quickly build and deploy an
+                    application using the database access service in a few
+                    simple steps.
+                  </Text>
+                </TextContent>
+                <Button
+                  iconPosition="right"
+                  icon={<ExternalLinkAltIcon />}
+                  target="_blank"
+                  className="pf-u-mt-xl"
+                  component="a"
+                  variant="secondary"
+                  href="https://youtu.be/wEcqQziu17o"
+                >
+                  View the demo
+                </Button>
+              </GridItem>
+              <GridItem md={6} sm={12}>
+                <iframe
+                  style={{ width: '100%', height: '315px' }}
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/wEcqQziu17o"
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </GridItem>
+            </Grid>
+          </StackItem>
+        </Stack>
       </Main>
     </React.Fragment>
   );
