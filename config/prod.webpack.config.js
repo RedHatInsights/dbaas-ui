94eac028580e98464793736a74be0d3d563f2621
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2023-08-17 11:28:49 +0200

    Add config to prod config

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -1,16 +1,23 @@
-const { resolve } = require('path');
-const config = require('@redhat-cloud-services/frontend-components-config');
-const commonPlugins = require('./plugins');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { resolve } = require("path");
+const config = require("@redhat-cloud-services/frontend-components-config");
+const commonPlugins = require("./plugins");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 const { config: webpackConfig, plugins } = config({
-  rootFolder: resolve(__dirname, '../'),
-  ...(process.env.BETA && { deployment: 'beta/apps' }),
+  rootFolder: resolve(__dirname, "../"),
+  definePlugin: {
+    TRUSTIFICATION_URL: JSON.stringify(
+      process.env.BETA
+        ? "https://staging.trustification.dev"
+        : "https://trustification.dev"
+    ),
+  },
+  ...(process.env.BETA && { deployment: "beta/apps" }),
 });
 plugins.push(...commonPlugins);
 
 module.exports = (env) => {
-  if (env && env.analyze === 'true') {
+  if (env && env.analyze === "true") {
     plugins.push(new BundleAnalyzerPlugin());
   }
   return {
