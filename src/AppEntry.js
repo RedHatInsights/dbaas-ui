cdbea0a62dd9635366dd26480902cc3207c568a3
Martin Marosi <marvusm.mmi@gmail.com>
2023-11-07 10:08:07 +0100

    fix: update router basename based on runtime environment

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { init } from './store';
 import App from './App';
 import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/helpers';
+import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 
-const basename = getBaseName(window.location.pathname);
-const AppEntry = () => (
-  <Provider store={init().getStore()}>
-    <Router basename={basename}>
-      <App basename={basename} />
-    </Router>
-  </Provider>
-);
+const AppEntry = () => {
+  const chrome = useChrome();
+  const basename = useMemo(
+    () => getBaseName(window.location.pathname, chrome.isBeta() ? 3 : 2),
+    []
+  );
+  return (
+    <Provider store={init().getStore()}>
+      <Router basename={basename}>
+        <App basename={basename} />
+      </Router>
+    </Provider>
+  );
+};
 
 export default AppEntry;
