f1587354789ed351ac55423eb03965e6712df818
Martin Marosi <marvusm.mmi@gmail.com>
2023-11-15 10:10:59 +0100

    Update config and dependencies.

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,39 @@
+const transformIgnorePatterns = ['node_modules/(?!(uuid)/)'];
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  coverageDirectory: './coverage/',
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js', '!src/**/stories/*'],
-  coverageDirectory: './coverage/',
+  roots: ['<rootDir>/src/'],
   moduleNameMapper: {
     '\\.(css|scss)$': 'identity-obj-proxy',
   },
-  roots: ['<rootDir>/src/'],
-  setupFiles: ['<rootDir>/config/setupTests.js'],
-  transformIgnorePatterns: [
-    '/node_modules/(?!@redhat-cloud-services)',
-    '/node_modules/(?!@patternfly)',
-  ],
+  transformIgnorePatterns,
+  setupFilesAfterEnv: ['<rootDir>/config/setupTests.js'],
+  transform: {
+    '^.+\\.(ts|js)x?$': [
+      '@swc/jest',
+      {
+        $schema: 'http://json.schemastore.org/swcrc',
+        jsc: {
+          experimental: {
+            plugins: [['jest_workaround', {}]],
+          },
+          parser: {
+            jsx: true,
+            syntax: 'typescript',
+            tsx: true,
+          },
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
+  },
 };
